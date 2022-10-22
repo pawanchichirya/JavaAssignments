bdd02ec94050c774cde70b99c7ac392455b73e3c
pawanchichirya <70518506+pawanchichirya@users.noreply.github.com>
2022-10-22 13:13:57 +0530

    Update Pattern.java

--- Assignment-1/Pattern.java
+++ Assignment-1/Pattern.java
@@ -44,7 +44,7 @@ public class Pattern {
 			
 			//R
 			for(int j=0;j<n;j++) {
-				if(j==0 || i==0 || i==(n-1)/2 || j==n-1 && i<=(n-1)/2 || i>(n-1)/2 && i-j==n/2) {
+				if(i==0 || j==0 || i==(n-1)/2 || i-j==(n-1)/2 -1 || j==n-1 && i<=(n-1)/2) {
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
