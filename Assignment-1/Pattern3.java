86c81a196c6d8dbce0840d32375fd41dc9df0538
pawanchichirya <70518506+pawanchichirya@users.noreply.github.com>
2022-10-22 13:36:06 +0530

    Update Pattern3.java

--- Assignment-1/Pattern3.java
+++ Assignment-1/Pattern3.java
@@ -1,7 +1,7 @@
 
 public class Pattern3 {
 	public static void main(String[] args) {
-		int n=15;
+		int n=14;
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
 				if(i==0 || i==n-1 || j==0 || j==n-1 || i+j<=(n-1)/2 || j-i>=(n-1)/2 ) {
